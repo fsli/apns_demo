fe20a1ad054d9d5d9611205a36ad28a60a992339
Ferris Li <fangsheng.ferris.li@gmail.com>
2016-03-28 22:30:57 -0700

    Upload new pem file

--- houston_run.rb
+++ houston_run.rb
@@ -3,16 +3,16 @@ require 'houston'
 # Environment variables are automatically read, or can be overridden by any specified options. You can also
 # conveniently use `Houston::Client.development` or `Houston::Client.production`.
 APN = Houston::Client.production
-APN.certificate = File.read("./aps_prod.pem")
+APN.certificate = File.read("./development_moteve_apns_sample.pem")
 
 #replace below token for each device
-token = "<2572e722 75d58aa0 79e8b13f 0e2f52ef aca1b8f2 a5ca08b8 a8dc3837 f2702fbb>"
+token = "<8c890871 ddc52968 c26095dc 8362ebf6 f2b73623 7424dc76 31ffbc89 943e1f8f>"
 # Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
 notification = Houston::Notification.new(device: token)
 notification.alert = "hello message"
 
 # Notifications can also change the badge count, have a custom sound, have a category identifier, indicate available Newsstand content, or pass along arbitrary data.
-notification.badge = 88
+notification.badge = 77
 
 # And... sent! That's all it takes.
 APN.push(notification)
